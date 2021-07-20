7ae4c1a598b927ec0a326fe75da880aefb065152
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-07-20 11:29:27 -0400

    setup and new ps copy

---
+++ drupal/om/sh/add_discharge_eqs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This adds new return flow structure as developed during spring of 2021
+pid=$1
+template=4988636
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "Usage: add_discharge_eqs.sh pid [template=$template]"
+  exit 2
+fi 
+
+if [ $# -gt 1 ]; then
+  template=$2
+fi 
+
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid ps_enabled;
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid ps_local_mgd;
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid ps_nextdown_mgd;
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid ps_other_mgd;
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid discharge_mgd; 

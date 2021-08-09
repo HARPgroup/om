770f2c12d5b0ca93a36c357d4d6b81d23ea83fc9
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-08-09 17:55:21 -0400

    load ds

--- R/summarize/waterSupplyElement.R
+++ R/summarize/waterSupplyElement.R
@@ -10,16 +10,17 @@ site <- "http://deq1.bse.vt.edu/d.dh"    #Specify the site of interest, either d
 basepath='/var/www/R';
 source(paste(basepath,'config.R',sep='/'))
 save_directory <-  "/var/www/html/data/proj3/out"
+library(hydrotools)
 # authenticate
 ds <- RomDataSource$new(site, rest_uname)
 ds$get_token(rest_pw)
+
 # Load Local libs
 library(stringr)
 library(ggplot2)
 library(sqldf)
 library(ggnewscale)
 library(dplyr)
-library(hydrotools)
 
 # Read Args
 argst <- commandArgs(trailingOnly=T)

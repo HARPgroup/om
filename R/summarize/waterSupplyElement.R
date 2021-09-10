3c541c74829589cf5e92fce06573fb9d45529657
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-09-10 08:14:01 -0400

    try splitting impoundment into dual plot

--- R/summarize/waterSupplyElement.R
+++ R/summarize/waterSupplyElement.R
@@ -556,47 +556,21 @@ if("impoundment" %in% cols) {
   png(fname)
   ymn <- 0
   ymx <- 100
+
   par(mar = c(5,5,2,5))
-  par(mar = c(1,5,2,5),mfrow = c(2,1))
   plot(
     datpd$storage_pct * 100.0,
-    ylim=c(0,100),
+    ylim=c(ymn,ymx),
+    main="Minimum Modeled Reservoir Storage Period",
     ylab="Reservoir Storage (%)",
-    xlab="",
-    main=paste("Storage and Flows",sdate,"to",edate)
+    xlab=paste("Model Time Period",l90_elev_start,"to",l90_elev_end)
   )
-  ymx <- ceiling(
-    pmax(
-      max(datpd$Qreach)
-    )
-  )
-  # if this is a pump store, refill_pump_mgd > 0
-  # then, plot Qreach first, overlaying impoundment_Qin
-  plot(
-    datpd$Qreach,
-    col='blue',
-    xlab="",
-    ylab='Flow/Demand (cfs)',
-    #ylim=c(0,ymx),
-    log="y",
-    yaxt="n" # supress labeling till we format
-  )
-  y_ticks <- axTicks(2)
-  y_ticks_fmt <- format(y_ticks, scientific = FALSE)
-  axis(2, at = y_ticks, labels = y_ticks_fmt)
-  ymx <- ceiling(
-    pmax(
-      max(datpd$refill_pump_mgd),
-      max(datpd$impoundment_demand * 1.547)
-    )
-  )
-  #par(new = TRUE)
-  #plot(datpd$refill_pump_mgd * 1.547,col='green',xlab="",ylab="")
-  lines(datpd$refill_pump_mgd * 1.547,col='red')
-  lines(datpd$impoundment_demand * 1.547,col='green')
-  #axis(side = 4)
-  #mtext(side = 4, line = 3, 'Flow/Demand (cfs)')
-
+  par(new = TRUE)
+  plot(datpd$impoundment_Qin,col='blue', axes=FALSE, xlab="", ylab="")
+  lines(datpd$Qout,col='green')
+  lines(datpd$wd_mgd * 1.547,col='red')
+  axis(side = 4)
+  mtext(side = 4, line = 3, 'Flow/Demand (cfs)')
   dev.off()
   print(paste("Saved file: ", fname, "with URL", furl))
   vahydro_post_metric_to_scenprop(scenprop$pid, 'dh_image_file', furl, 'fig.l90_imp_storage', 0.0, ds)

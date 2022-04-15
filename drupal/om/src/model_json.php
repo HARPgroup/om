bc0c3261cc2d37435237ba8628ce6216cfd56a68
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-04-15 10:03:25 -0400

    get plugin

--- drupal/om/src/model_json.php
+++ drupal/om/src/model_json.php
@@ -7,11 +7,15 @@ while ($arg = drush_shift()) {
   $args[] = $arg;
 }
 $pid = $args[0];
+$slashes = $arg[1];
 $model = entity_load_single('dh_properties', $pid);
 $plugin = dh_variables_getPlugins($model);
 $plugin->loadProperties($model);
 $exp = $plugin->exportOpenMI($model);
-$exp_json = addslashes(json_encode($exp[$model->propname]));
+$exp_json = $exp[$model->propname];
+if ($slashes === 1) {
+  $exp_json = addslashes(json_encode($exp_json));
+}
 echo $exp_json;
 
 ?>

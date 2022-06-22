25c7cf018a64c7c1def10ea5b9b31e5d89a8825b
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-06-22 16:55:14 -0400

    set up proper handling of equaitions in embedded forms, added small wateshed test

--- drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
@@ -1664,7 +1664,7 @@ class dHOMEquation extends dHOMSubComp {
   
   public function attachNamedForm(&$form, $entity) {
     $varinfo = $entity->varid ? dh_vardef_info($entity->varid) : FALSE;
-    dpm($varinfio,'var infi');
+    dpm($varinfo,'var info');
     if (!$varinfo) {
       return FALSE;
     }
@@ -1676,7 +1676,7 @@ class dHOMEquation extends dHOMSubComp {
     $form[$mname] = $pform['propcode'];
     $form[$mname]['#title'] = isset($entity->title) ? t($entity->title) : t($entity->propname);
     $form[$mname]['#description'] = t($entity->vardesc);
-    dpm($form[$mname],'var infi');
+    dpm($form[$mname],'form field: ' . $mname);
   }
   
   public function setAllRemoteProperties($entity, $elid, $path) {

7d7151bdd074b3fc735f0c45c060e9a9b6d9e9a6
Robert W. Burgholzer <rburghol@vt.edu>
2022-06-24 19:53:32 +0000

    add a quick local lib script

---
+++ sh/use_local_drupal.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# link drupal module in to this machines repo
+# this is how it *should* be on the live server, but the dev server, since 
+# it is a backup of live, often relies on the rsync code-base, but there is not need
+rm /var/www/html/d.dh/modules/om
+ln -s /opt/model/om/drupal/om /var/www/html/d.dh/modules/om
+
+

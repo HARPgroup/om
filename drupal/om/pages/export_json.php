08dfea3df87b3e65a03534dc17a186a1aee186c7
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-11-09 14:52:22 -0500

    supress run results if requested

--- drupal/om/pages/export_json.php
+++ drupal/om/pages/export_json.php
@@ -3,12 +3,13 @@
 module_load_include('module', 'om');
 $a = arg();
 $pid = NULL;
-
+$no_results = 0;
 if (isset($a[2])) {
   $pid = $a[2];
   $mode = $a[3];
+  $no_results = $a[4];
 } else {
-  echo "Usage: /[pid]/mode(json,single_json,vardef,debug)/no_results(0,1)<br>";
+  echo "Usage: /[pid]/mode(json,single_json,vardef,debug)/no_results(0,1)<br><br>";
 }
 if ($pid <> NULL) {
   $prop = entity_load_single('dh_properties', $pid);
@@ -28,9 +29,10 @@ if ($pid <> NULL) {
     }
   }
 
-  // tis may no longer be used, as drupal_export_json handles it?  But we *could* use this if it is better.
+  // tis may no lionger be used, as drupal_export_json handles it?  But we *could* use this if it is better.
   $exp_json = json_encode($exp, JSON_PRETTY_PRINT);
   $vars_json = json_encode($vars, JSON_PRETTY_PRINT);
+
   switch ($mode) {
     case 'vardef':
       drupal_json_output($vars);
@@ -44,8 +46,9 @@ if ($pid <> NULL) {
       // this asks for bare json export, not encapsulated as a self-named component 
       $exp = $exp[$prop->propname];
     case 'json':
-      // this leaves the output encapsulated in a named array, useful for outputting 
-      // multiple objects in a single export for eample if we were loading out a full model run
+    // this leaves the output encapsulated in a named array, useful for outputting
+    // multiple objects in a single export for eample if we were loading out a full model run
+
     default:
       drupal_json_output($exp);
       drupal_exit();

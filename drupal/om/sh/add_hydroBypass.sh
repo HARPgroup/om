0111c352febc083576bc1976cd6d6743ac1e5c88
Robert W. Burgholzer <rburghol@vt.edu>
2023-12-01 19:27:23 +0000

    added script to copy default hydropower components

---
+++ drupal/om/sh/add_hydroBypass.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pid=$1
+template=4988636
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "Usage: add_hydroBypass.sh model_pid [template_entity_id]" >&2
+  exit 2
+fi 
+
+if [ $# -gt 1 ]; then
+  template=$2
+fi 
+
+
+for i in Qbypass Qavail_divert Qturbine_max Qturbine; do
+  echo "drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid $i"
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid $i
+done
+

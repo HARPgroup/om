af24d5958b734cdbdafae78b820f53b9ff80e9e0
Robert W. Burgholzer <rburghol@vt.edu>
2024-02-14 02:23:37 +0000

    new better flowby

--- drupal/om/sh/set_impoundment_release_listener.sh
+++ drupal/om/sh/set_impoundment_release_listener.sh
@@ -26,5 +26,7 @@ echo "Deleting old refill_max_mgd (it it exists) since it is included in the Res
 drush scr modules/om/src/om_deleteprop.php cmd dh_properties $pid refill_max_mgd
 echo "Add refill_allowed_mgd which is set to listen to refill_max_mgd from broadcast"
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid refill_allowed_mgd
+echo "Copt available_mgd which is set to use refill_flowby from broadcast"
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid available_mgd
 
 echo "Properties have been copied.  Note: if this is a small trib with impoundment, you must edit the impoundment sub-component and set release = release_cfs (default is zero)"

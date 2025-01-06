effb4dc0407b52f74e0e794515e9d0f88b6c0907
Robert W. Burgholzer <rburghol@vt.edu>
2025-01-06 16:47:20 +0000

    more props for imps

--- drupal/om/sh/set_impoundment_release_rules.sh
+++ drupal/om/sh/set_impoundment_release_rules.sh
@@ -17,9 +17,15 @@ fi
 template=4988636
 
 
+# Set destination model to not write
+drush scr modules/om/src/om_setprop.php cmd dh_properties $pid om_element_connection om_element_connection $elementid 0
+
 # make sure it is using the new discharge_mgd variable 
-for i in release release_current release_historic release_proposed; do
+for i in release release_current release_historic release_proposed refill_transfer_available_mgd refill_transfer_max_mgd; do
   drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid $i
 done
+
+# Restore destination write setting and save
+drush scr modules/om/src/om_setprop.php cmd dh_properties $pid om_element_connection om_element_connection $elementid 1
 
 print "Finished. Note, if you wish to control a remote impoundment, you must also run set_impoundment_release_sender.sh"

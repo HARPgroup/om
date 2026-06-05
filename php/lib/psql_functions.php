a457a92e45c088748fce7aada7e8e5289bcc6e51
Rob <rburghol@vt.edu>
2026-06-05 19:10:41 +0000

    Fixed array error log

--- php/lib/psql_functions.php
+++ php/lib/psql_functions.php
@@ -326,8 +326,8 @@ class pgsql_QueryObject {
               # screen for array values, which we should NOT be getting.
               if (is_array($thisval)) {
                 error_log("Fatal Error: Array value sent to SQL logging for $thisname - Quitting");
-                error_log("Debugging Info: array keys:", print_r($colnames,1));
-                error_log("Debugging Info: array values:", print_r($values[$firstkey],1));
+                error_log("Debugging Info: array keys:" . print_r($colnames,1));
+                error_log("Debugging Info: array values:" . print_r($values[$firstkey],1));
                 exit;
               }
 
